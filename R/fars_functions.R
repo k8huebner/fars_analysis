8b2f651477f19a11f9090e9bc66dbafdbbc1d0d4
Maggie Weinroth <weinroth@rams.colostate.edu>
2017-11-05 14:51:12 -0700

    updated funcition CI

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -2,12 +2,16 @@ perc_cis <- function(x, n) {
   
   prob <- (x / n)
   SE <- sqrt((prob*(1-prob)/n))
-  CI_lower <- prob + 1.96 * (SE*prob)
-  CI_upper <- prob - 1.96 * (SE*prob)
+  CI_lower <- prob - (1.96 * (SE*prob))
+  CI_upper <- prob + (1.96 * (SE*prob))
   
   CI <- c(CI_lower, CI_upper)
   CI
+  print_out <- c(prob, CI)*100
+  print_out <- round(print_out, digits = 1)
+  print_out 
 }
 
 
 perc_cis(x = 9000, n = 23000)
+
