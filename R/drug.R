8e39965b95ee10fa5421adb8de1309a00b340113
Maggie Weinroth <weinroth@rams.colostate.edu>
2017-11-05 16:59:32 -0700

    update

--- R/drug.R
+++ R/drug.R
@@ -1,33 +1,6 @@
-library(tidyverse)
-to_test <- clean_fars %>%
-  filter(drug_type == "Alcohol")
-log_reg <- glm(positive_for_drug ~ year, data = to_test,
-               family = binomial(link = "logit"))
-summary(log_reg)$coefficients
+#third function
 
-clean_fars1 <- clean_fars %>% 
-  mutate(Alcohol = drug_type != "Alcohol")
-if drug == "Nonalcohol"
-else drug == c("Alcohol, Cannabinoid", "Depressant", "Narcotic", "Other", "Stimulant")
-
-
-<- function(){
-  # Print out today's date
-  cat("Today's date is: ")
-  cat(format(Sys.time(), "%b %d, %Y."), "\n")
-  # Add something based on the weekday of today's date
-  todays_wday <- lubridate::wday(Sys.time())
-  if(todays_wday %in% c(1, 7)){ # What to do on Sat / Sun
-    cat("It's the weekend!")
-  } else if (todays_wday == c(6)) { # What to do on Friday
-    cat("It's almost the weekend!")
-  } else { # What to do other days
-    cat("It's ", 7 - todays_wday, "days until the weekend.")
-  }
-}
-
-
-<- function(drug, df){
+functionmaybe <- function(drug, df = clean_fars){
   if(drug == "Nonalcohol") {
     
     nonalcohol <- df %>% 
@@ -35,17 +8,21 @@ else drug == c("Alcohol, Cannabinoid", "Depressant", "Narcotic", "Other", "Stimu
       filter(alchyesno == "TRUE") %>% 
       select("unique_id", "sex", "year", "agecat", "positive_for_drug") %>% 
       mutate(drug_type = "Nonalcohol")
+    log_reg <- glm(positive_for_drug ~ year, data = to_test,
+                   family = binomial(link = "logit"))
+    summary(log_reg)$coefficients
+    
     
   }
-  if(df$drug %in% c("Alcohol, Cannabinoid", "Depressant", 
-                 "Narcotic", "Other", "Stimulant"){
-    
+  else {
+    to_test <- df %>%
+      filter(drug_type == drug)
+    log_reg <- glm(positive_for_drug ~ year, data = to_test,
+                   family = binomial(link = "logit"))
+    summary(log_reg)$coefficients
   }
-   
 }
+   
 
+functionmaybe(drug = "Other")
 
-
-  
-
-summary(test)
