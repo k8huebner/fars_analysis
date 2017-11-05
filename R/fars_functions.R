05c93635f15214e960b1db2b191b8c9b5d76c2a7
Kate Huebner <kate.huebner@colostate.edu>
2017-11-05 15:24:09 -0700

    updated function

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -5,13 +5,12 @@ perc_cis <- function(x, n) {
   CI_lower <- prob - (1.96 * (SE*prob))
   CI_upper <- prob + (1.96 * (SE*prob))
   
-  CI <- c(CI_lower, CI_upper)
-  CI
-  print_out <- c(prob, CI)*100
-  print_out <- round(print_out, digits = 1)
-  print_out 
+  CI_lower_perc <- round(CI_lower*100, digits = 1)
+  CI_upper_perc <- round(CI_upper*100, digits = 1)
+  prob_perc <- round(prob*100, digits = 1)
+  print_out <- paste0(prob_perc, "%", " (", CI_lower_perc, "%",", ", CI_upper_perc, "%", ")")
+  print_out
 }
 
-
-perc_cis(x = 9000, n = 23000)
-
+numbers <- perc_cis(x = 9000, n = 23000)
+numbers
