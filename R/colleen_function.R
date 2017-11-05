8f2c36f4ca1da47dad278075a275d75e8d9f4310
Colleen Brents <cbrents1@rams.colostate.edu>
2017-11-05 16:17:45 -0700

    Colleen's stab at function

--- R/colleen_function.R
+++ R/colleen_function.R
@@ -12,27 +12,25 @@ ca_alcohol <- prop.trend.test(x = to_test$positive,
 #reports Z^2 and p-value 
 sqrt(ca_alcohol$statistic)
 #reports Z
-
 ca_alcohol$p.value
 
-test <- function(data = clean_fars, drug_type = "Alcohol"){
-  filter(drug_type == "drug") %>% 
-  group_by (year) %>% 
-  summarize(positive = sum(positive_for_drug, na.rm = TRUE),
-              trials = sum(!is.na(positive_for_drug)))
-  ca_alcohol <- prop.trend.test(x = to_test$positive,
-                                n = to_test$trials)
+print()
+
+
+function(drug,df){
+   if(df$drug == c("Alcohol", "Cannabinoid", "Depressant", "Narcotic", "Other", "Stimulant")){
+     
+   }
+ if(df$drug == "Nonalcohol"){
+  Nonalcohol <- c("Cannabinoid", "Depressant", "Narcotic", "Other", "Stimulant") 
+  
+  
 }
 
-# = 
-perc_cis <- function(x, n) {
+
   
-  prob <- (x / n)
-  SE <- sqrt((prob*(1-prob)/n))
-  CI_lower <- prob + 1.96 * (SE*prob)
-  CI_upper <- prob - 1.96 * (SE*prob)
-  
-  CI <- c(CI_lower, CI_upper)
-  CI
-  
-}
+
+
+lapply 
+#to make table 
+
